255e20020cfd56c026aa4e4ce35fbe73500c61af
mckeny3 <84984030+mckeny3@users.noreply.github.com>
2023-05-21 18:41:43 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const NavView = () => {
+  return (
+    <View>
+      <Text>NavView</Text>
+    </View>
+  )
+}
+
+module.exports = NavView
+
 
